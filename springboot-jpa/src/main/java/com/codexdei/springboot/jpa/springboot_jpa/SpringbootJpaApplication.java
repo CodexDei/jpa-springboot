995e883184a44081e91911132607b9b3bca3b040
codexdei <jlacostc@gmail.com>
2026-01-15 21:02:08 -0500

    se agregan los metodos list(), create() y update() a la clase SpringbootJpaApplication

--- springboot-jpa/src/main/java/com/codexdei/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/codexdei/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,18 +1,22 @@
 package com.codexdei.springboot.jpa.springboot_jpa;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.codexdei.springboot.jpa.springboot_jpa.entities.Person;
 import com.codexdei.springboot.jpa.springboot_jpa.repositories.PersonRepository;
 
 @SpringBootApplication
-//Se implementa CommandLineRUnner para que se pueda manejar como aplicacion de consola
-public class SpringbootJpaApplication implements CommandLineRunner{
+// Se implementa CommandLineRUnner para que se pueda manejar como aplicacion de
+// consola
+public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Autowired
 	private PersonRepository repository;
@@ -24,15 +28,141 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		//Listar toda la tabla persons
-		//List<Person> persons = (List<Person>) repository.findAll();
-		//List<Person> programinList = repository.findByProgrammingLanguage("Java");
-		//List<Person> nombreLenguageLista = repository.buscarPorNameYLenguajeProgramacion("Yorking","Java");
+/* 		list();
+		findOne();
+		create(); */
+		update();
+	}
+
+	// @Transactional envuelve todo el metodo en una transaccion, es decir todas
+	// deben ejecutarse correctamente
+	// Para que se aplique, si una falla no se ejecuta ninguna
+	// Se usa de esta manera para Consultas, es decir busquedas de informacion,
+	// listar, select, etc
+	@Transactional(readOnly = true)
+	public void findOne() {
+
+		// usando metodo con query personalizado
+		repository.findOne(3L).ifPresent(p -> System.out.println(p));
+		repository.findOneName("Dix").ifPresent(p -> System.out.println(p));
+		// repository.findOneLikeName("ey").ifPresent(p -> System.out.println(p));
+		// usando metodo gestionado por nombre de springboot
+		// repository.findById(1L).ifPresent(p -> System.out.println(p));
+		// repository.findByName("Robin").ifPresent(p -> System.out.println(p));
+		repository.findByNameContaining("Robin").ifPresent(p -> System.out.println(p));
+	}
+
+	// @Transactional envuelve todo el metodo en una transaccion, es decir todas
+	// deben ejecutarse correctamente
+	// Para que se aplique, si una falla no se ejecuta ninguna
+	// Se usa de esta manera para Consultas, es decir busquedas de informacion,
+	// listar, select, etc
+	@Transactional(readOnly = true)
+	public void list() {
+
+		// Listar toda la tabla persons
+		// List<Person> persons = (List<Person>) repository.findAll();
+		// List<Person> programinList = repository.findByProgrammingLanguage("Java");
+		// List<Person> nombreLenguageLista =
+		// repository.buscarPorNameYLenguajeProgramacion("Yorking","Java");
 		List<Object[]> apellidoLenguageLista = repository.obtenerDatosPersona();
 
-		//programinList.stream().forEach(System.out::println);
-		//nombreLenguageLista.stream().forEach(System.out::println);
+		// programinList.stream().forEach(System.out::println);
+		// nombreLenguageLista.stream().forEach(System.out::println);
 		apellidoLenguageLista.stream().forEach(p -> System.out.println("Apellido: " + p[0] + " Lenguage: " + p[1]));
+	}
+
+	// @Transactional envuelve todo el metodo en una transaccion, es decir todas
+	// deben ejecutarse correctamente
+	// Para que se aplique, si una falla no se ejecuta ninguna
+	// A diferencia de las consultas se usa sin 'readOnly' porque en este caso no
+	// son consultas, sino
+	// operaciones que modifican las tablas
+	@Transactional
+	public void create() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter name:");
+		String name = scanner.next();
+
+		System.out.println("Enter lastname:");
+		String lastname = scanner.next();
+
+		System.out.println("Enter programming Language:");
+		String programmigLanguage = scanner.next();
+
+		scanner.close();
+
+		Person person = new Person(null, name, lastname, programmigLanguage);
+		Person newPerson = repository.save(person);
+
+		repository.findById(newPerson.getId()).ifPresent(System.out::println);
+	}
+
+	@Transactional
+	public void update() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		list();
+		System.out.println("Enter the ID to search:");
+		Long id = scanner.nextLong();
+		//limpiar buffer
+		scanner.nextLine();
+
+		Optional<Person> persOptional = repository.findById(id);
+
+		if (persOptional.isPresent()) {
+
+			Person person = persOptional.orElseThrow();
+
+			System.out.println(person);
+
+			System.out.println("Enter the number of the date to update:");
+			System.out.println("1. Name");
+			System.out.println("2. Lastname");
+			System.out.println("3. Programming Language");
+			System.out.print("Enter number:");
+			int optionNumber = scanner.nextInt();
+			//limpiar buffer
+			scanner.nextLine();
+			
+			switch (optionNumber) {
+
+				case 1 -> {
+
+					System.out.println("Enter new name:");
+					String name = scanner.nextLine();
+					person.setName(name);
+					repository.save(person);
+				}
+				case 2 -> {
+
+					System.out.println("Enter new lastname:");
+					String lastname = scanner.nextLine();
+					person.setLastname(lastname);
+					repository.save(person);
+				}
+				case 3 -> {
+
+					System.out.println("Enter new programming language:");
+					String programmingLanguage = scanner.nextLine();
+					person.setProgrammingLanguage(programmingLanguage);
+					repository.save(person);
+				}
+
+				default -> {
+					System.out.println("The entered option is incorrect");
+				}
+			}
+			
+			System.out.println(person);
+		
+		}else{
+
+			System.out.println("The ID entered does not exist");
+		}
 	}
 
 }
--- springboot-jpa/src/main/java/com/codexdei/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/codexdei/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -5,17 +5,34 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.codexdei.springboot.jpa.springboot_jpa.entities.Person;
 import java.util.List;
+import java.util.Optional;
 
 
 //Los datos genericos: el primero hace referencia a la entidad y el segundo a tipo de dato del id, que 
 //por lo regular es Long
 public interface PersonRepository extends CrudRepository<Person,Long>{
 
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    //busqueda por coincidencia, es decir empiece por o termine por determinada letra(s) o vocal(s)
+    @Query("select p from Person p where p.name like %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+
     //Aqui en nombre que demos al metodo si es importante, ya que tiene que ser en ingles, usar preferiblemente
     //el que sugiera el ID, ya que es un estandar e internamente ejecutara el Query, si por ejemplo
     //le colocamos por nombre al metodo buscarPorLenguageProgramacion no lo reconocera, a menos que nosotros
-    //creemos el Query con la anotacion @Query como se aprecia en la segunda linea de codigo
+    //creemos el Query con la anotacion @Query como se aprecia mas abajo
     List<Person> findByProgrammingLanguage(String programmingLanguage);
+    //si usamos el nombre estandar del nombre del metodo ya tiene implementado internamente el Query
+    Optional<Person> findByName(String name);
+    //buscando por coincidencia, si le agregamos 'Containing' al nombre del metodo es como si hicieramos un
+    //like
+    Optional<Person> findByNameContaining(String name);
+    
     //Usando Query y digitando nosotros directamente el Query SOLO en ese caso podemos colocarle
     //cualquier nombre
     @Query("select p from Person p where p.name=?1 and p.programmingLanguage=?2")
